503b18747558e9724b38c2136c8c5f445add180d
Pierre Emmanuel DOUET <tathar.dev@gmail.com>
2021-12-08 19:04:30 +0100

    pt100

--- lib/DT_PT100/DT_PT100.cpp
+++ lib/DT_PT100/DT_PT100.cpp
@@ -3,7 +3,7 @@
 
 #include <Adafruit_MAX31865.h>
 
-Adafruit_MAX31865 temp[2] = {Adafruit_MAX31865(10), Adafruit_MAX31865(12)};
+Adafruit_MAX31865 temp[2] = {Adafruit_MAX31865(11), Adafruit_MAX31865(12)};
 
 float old_temp[TEMP_NUM];
 void (*pt100_callback)(const uint8_t num, const float temp);
@@ -22,30 +22,30 @@ float _temp_get(int num)
         Serial.println(fault, HEX);
         // sprintf(buffer, "with %%p:  x    = %p\n", &capteur);
         // Serial.print(buffer);
-        // if (fault & MAX31865_FAULT_HIGHTHRESH)
-        // {
-        //     Serial.println("RTD High Threshold");
-        // }
-        // if (fault & MAX31865_FAULT_LOWTHRESH)
-        // {
-        //     Serial.println("RTD Low Threshold");
-        // }
-        // if (fault & MAX31865_FAULT_REFINLOW)
-        // {
-        //     Serial.println("REFIN- > 0.85 x Bias");
-        // }
-        // if (fault & MAX31865_FAULT_REFINHIGH)
-        // {
-        //     Serial.println("REFIN- < 0.85 x Bias - FORCE- open");
-        // }
-        // if (fault & MAX31865_FAULT_RTDINLOW)
-        // {
-        //     Serial.println("RTDIN- < 0.85 x Bias - FORCE- open");
-        // }
-        // if (fault & MAX31865_FAULT_OVUV)
-        // {
-        //     Serial.println("Under/Over voltage");
-        // }
+        if (fault & MAX31865_FAULT_HIGHTHRESH)
+        {
+            Serial.println("RTD High Threshold");
+        }
+        if (fault & MAX31865_FAULT_LOWTHRESH)
+        {
+            Serial.println("RTD Low Threshold");
+        }
+        if (fault & MAX31865_FAULT_REFINLOW)
+        {
+            Serial.println("REFIN- > 0.85 x Bias");
+        }
+        if (fault & MAX31865_FAULT_REFINHIGH)
+        {
+            Serial.println("REFIN- < 0.85 x Bias - FORCE- open");
+        }
+        if (fault & MAX31865_FAULT_RTDINLOW)
+        {
+            Serial.println("RTDIN- < 0.85 x Bias - FORCE- open");
+        }
+        if (fault & MAX31865_FAULT_OVUV)
+        {
+            Serial.println("Under/Over voltage");
+        }
         temp[num].clearFault();
         return -273.15;
     }
@@ -66,16 +66,16 @@ void DT_pt100_init()
     for (uint8_t num = 0; num < TEMP_NUM; ++num)
     {
         old_temp[num] = 0;
-        uint8_t pin = pgm_read_byte(TEMP_ARRAY + num);
-        pinMode(pin, OUTPUT);
-        digitalWrite(pin, HIGH);
+        // uint8_t pin = pgm_read_byte(TEMP_ARRAY + num);
+        // pinMode(pin, OUTPUT);
+        // digitalWrite(pin, HIGH);
         temp[num].begin(MAX31865_3WIRE);
     }
 }
 
 void DT_pt100_loop()
 {
-    delay(50);
+    // delay(50);
     static uint32_t old_time = 0;
     uint32_t now = millis();
     float tmp = 0;
@@ -85,7 +85,11 @@ void DT_pt100_loop()
         for (uint8_t num = 0; num < 2; num++)
         {
             tmp = _temp_get(num);
-            if (old_temp[num] != tmp)
+            if (tmp < -100 || tmp > 200)
+            {
+                temp[num].clearFault();
+            }
+            else if (old_temp[num] != tmp)
             {
                 old_temp[num] = tmp;
                 if (pt100_callback != nullptr)
