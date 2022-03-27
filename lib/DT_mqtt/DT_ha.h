bc956832e73aa063486839ecb4c4950fd6ddb2cb
Pierre Emmanuel DOUET <tathar.dev@gmail.com>
2022-03-27 23:39:23 +0200

    Create DT_HA.c and .h

---
+++ lib/DT_mqtt/DT_ha.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+#include <avr/wdt.h> //watchdog
+
+#include <config.h>
+#include <DT_mqtt.h>
+#include <ArduinoJson.h>
+
+
+extern StaticJsonDocument<256> doc;
+extern char buffer[BUFFER_SIZE];
+extern char buffer_value[BUFFER_VALUE_SIZE];
+
+
+void homeassistant(void);
